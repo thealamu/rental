c6e4f7734cf71bd74c062ba13d48d776c4d1cdee
Faithfulness Alamu <vaguemail369@gmail.com>
2020-08-01 16:30:43 +0100

    Set port flag

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	flag "github.com/spf13/pflag"
+)
+
+var port string
+
+func main() {
+	parseFlags()
+	if port == "" {
+		//Check if port is set in the environment
+		if port = os.Getenv("PORT"); port == "" {
+			// It's not, default to 8080
+			port = "8080"
+		}
+	}
+	fmt.Println(port)
+}
+
+func parseFlags() {
+	flag.StringVarP(&port, "port", "p", "", "port to start the server on")
+	flag.Parse()
 }
