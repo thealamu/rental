8ca0c120d4abc901e38e3cd28cc005267a71b39d
Faithfulness Alamu <vaguemail369@gmail.com>
2020-08-02 14:57:05 +0100

    Create default db config

--- db.go
+++ db.go
@@ -8,14 +8,35 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-type database struct {
-	gormDB *gorm.DB
-}
-
 //configurations for the database
 type dbconfig struct {
 	dialect string
 	dbURI   string
+}
+
+var defaultDbConfig = &dbconfig{
+	dialect: "mysql",
+	dbURI:   os.ExpandEnv("${RTL_USER}:${RTL_PASS}@/${RTL_DB}?charset=utf8mb4&parseTime=True&loc=Local"),
+}
+
+var (
+	once sync.Once
+	gdb  *gorm.DB
+)
+
+type database struct {
+	gormDB *gorm.DB
+}
+
+//newDatabase returns a new database object using the dialect in the config;
+//it defaults to mysql. All database objects use the same underlying gorm db
+func newDatabase(config *dbconfig) (ret database, err error) {
+	ret = database{}
+	once.Do(func() {
+		err = setupGDB(config.dialect, config.dbURI)
+	})
+	ret.gormDB = gdb
+	return
 }
 
 //listPublicCars returns all cars made public by merchants
@@ -30,26 +51,6 @@ func (d database) close() {
 	d.gormDB.Close()
 }
 
-var (
-	once sync.Once
-	gdb  *gorm.DB
-)
-
-//newDatabase returns a new database object using the dialect in the config;
-//it defaults to mysql. All database objects use the same underlying gorm db
-func newDatabase(config *dbconfig) (ret database, err error) {
-	ret = database{}
-	once.Do(func() {
-		if config == nil {
-			//default config
-			config = defaultConfig()
-		}
-		err = setupGDB(config.dialect, config.dbURI)
-	})
-	ret.gormDB = gdb
-	return
-}
-
 //setupGDB does init of the gorm db
 func setupGDB(dialect, dbURI string) error {
 	conn, err := gorm.Open(dialect, dbURI)
@@ -60,13 +61,4 @@ func setupGDB(dialect, dbURI string) error {
 	gdb.AutoMigrate(&car{})
 
 	return nil
-}
-
-//defaultConfig returns a mysql config
-func defaultConfig() *dbconfig {
-	uri := os.ExpandEnv("${RTL_USER}:${RTL_PASS}@/${RTL_DB}?charset=utf8mb4&parseTime=True&loc=Local")
-	return &dbconfig{
-		dbURI:   uri,
-		dialect: "mysql",
-	}
 }
