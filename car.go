df807ab4c891859795b57ff9f0a4be29d784d141
Faithfulness Alamu <vaguemail369@gmail.com>
2020-08-02 12:47:42 +0100

    Update car models

--- car.go
+++ car.go
@@ -4,9 +4,32 @@ import "github.com/jinzhu/gorm"
 
 type car struct {
 	gorm.Model
-	Name        string
-	Description string
-	Charge      charge
+	Name         string
+	Description  string
+	Charge       charge
+	Make         string
+	Transmission string
+	SeatCount    int
+	ImgURL       string
+	Color        string
+	AC           bool
+	Electric     bool
+	Convertible  bool
+}
+
+type pubCar struct {
+	ID           uint
+	Name         string
+	Description  string
+	Charge       charge
+	Make         string
+	Transmission string
+	SeatCount    int
+	ImgURL       string
+	Color        string
+	AC           bool
+	Electric     bool
+	Convertible  bool
 }
 
 type charge struct {
