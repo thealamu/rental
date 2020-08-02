a493b57d39552a398577550cbc85ab8888c91f2f
Faithfulness Alamu <vaguemail369@gmail.com>
2020-08-02 14:49:27 +0100

    Use sqlite in dbtest

--- db_test.go
+++ db_test.go
@@ -1,19 +1,23 @@
 package main
 
 import (
-	"os"
 	"testing"
+
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 func TestNewDB(t *testing.T) {
-	//setup test env
-	os.Setenv("RTL_DB", "rental")
-	os.Setenv("RTL_USER", "rental")
-	os.Setenv("RTL_PASS", "rental")
+	var c dbconfig
+	c.dialect = "sqlite3"
+	c.dbURI = "file::memory:?cache=shared"
 
-	db := newDatabase()
+	db, err := newDatabase(&c)
+	if err != nil {
+		t.Error(err)
+	}
 	if db.gormDB == nil {
 		t.Errorf("Expected returned database to have a gormDB, got nil")
+		return
 	}
 
 	db.close()
