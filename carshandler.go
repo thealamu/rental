f5611ca5f54d21a5382bc70b19961584eff07840
Faithfulness Alamu <vaguemail369@gmail.com>
2020-08-02 13:26:50 +0100

    Implement carshandler

--- carshandler.go
+++ carshandler.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
 //carsHandler serves path /cars
 func carsHandler(w http.ResponseWriter, r *http.Request) {
 	db := newDatabase()
+	pubCars, err := db.listPublicCars()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	data, err := json.Marshal(pubCars)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	w.Write(data)
 }
