40c8a46db7cca0d48f26d9cdab27d4e16d1cc7c3
Faithfulness Alamu <vaguemail369@gmail.com>
2020-08-07 12:59:11 +0100

    Implement the logout handler

---
+++ logouthandler.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func handleLogout(w http.ResponseWriter, r *http.Request) {
+	tag := "handler.logout"
+
+	logoutURL, err := newLogoutURL(r)
+	if err != nil {
+		respondError(tag, w, failCodeUnknown, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	email, err := getProfileValue(r, "email")
+	if err != nil {
+		respondError(tag, w, failCodeSessionDB, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("%s: %s logged out", tag, email)
+
+	http.Redirect(w, r, logoutURL.String(), http.StatusTemporaryRedirect)
+}
