6d83106fd2c7b3de1b08931b13812e09c93effc9
Faithfulness Alamu <vaguemail369@gmail.com>
2020-08-07 10:55:20 +0100

    Add cars search url to common endpoints

--- roothandler.go
+++ roothandler.go
@@ -6,11 +6,13 @@ import (
 
 var commonEndpoints = struct {
 	Car             string `json:"car_url"`
+	CarsSearch      string `json:"cars_search_url"`
 	MerchantAccount string `json:"merchant_account_url"`
 	Merchant        string `json:"merchant_url"`
 	Login           string `json:"login_url"`
 }{
 	Car:             "/cars/{car_id}",
+	CarsSearch:      "/cars/search",
 	MerchantAccount: "/merchants/me",
 	Merchant:        "/merchants/{merchant}",
 	Login:           "/auth/login",
