13eed12b1e39a6c95fe0a79fd3db700c8d3c1b80
Faithfulness Alamu <vaguemail369@gmail.com>
2020-08-07 12:20:31 +0100

    Add customer account url to common endpoints

--- roothandler.go
+++ roothandler.go
@@ -7,11 +7,13 @@ import (
 var commonEndpoints = struct {
 	Car             string `json:"car_url"`
 	MerchantAccount string `json:"merchant_account_url"`
+	CustomerAccount string `json:"customer_account_url"`
 	Merchant        string `json:"merchant_url"`
 	Login           string `json:"login_url"`
 }{
 	Car:             "/cars/{car_id}",
 	MerchantAccount: "/merchants/me",
+	CustomerAccount: "/customers/me",
 	Merchant:        "/merchants/{merchant}",
 	Login:           "/auth/login",
 }
